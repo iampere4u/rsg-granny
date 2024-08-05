305f1b21914cd1ea6101f4f9e2b625ef4169ebbd
I AM PERE <75316668+iampere4u@users.noreply.github.com>
2024-08-05 14:49:58 +0530

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,16 +1,16 @@
-fx_version 'cerulean'
-games {'rdr3'}
+fx_version "cerulean"
+games {"rdr3"}
 rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
 
 description 'rsg-granny'
 version '2.0'
 
 client_scripts {
-    'client.lua'
+    'client/client.lua'
 }
 
 server_scripts {
-    'server.lua'
+    'server/server.lua'
 }
 
 shared_scripts {
@@ -25,3 +25,4 @@ dependencies {
 }
 
 lua54 'yes'
+
